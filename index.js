8abeade013d8888db0e8ad38c8e008f464f08016
upluke <lukeforadream@gmail.com>
2019-05-07 01:01:54 -0700

    some change

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express =require('express');
+const mongoose =require('mongoose');
+const keys=require('./config/keys')
+require('./models/User')
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
+
 const app=express();
 
-app.get('/',(req,res)=>{
-  res.send({Dumbonu:'When you are dumb, your mom will be worried'});
-})
+require('./routes/authRoutes')(app);
+
+
 const PORT =process.env.PORT||5000;
 
 app.listen(PORT);
+
+
+//finished 39s
