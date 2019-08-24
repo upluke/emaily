3fc82a4594d3565dda93ad2bf0447f7b18fba2ce
upluke <lukeforadream@gmail.com>
2019-08-23 22:09:41 -0700

    10908232019

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail{
     constructor({subject,recipients},content){
       super();
 
+      this.sgApi = sendgrid(keys.sendGridKey);
       this.from_email=new helper.Email('no-reply@emaily.com');
       this.subject=subject;
       this.body=new helper.Content('text/html',content);
@@ -39,5 +40,14 @@ class Mailer extends helper.Mail{
     this.addPersonalization(personalize);
   }
 
+  async send(){
+    const request =this.sgApi.emptyRequest({
+      method:'POST',
+      path:'/v3/mail/send',
+      body: this.toJSON()
+    });
+    const response =this.sgApi.API(request);
+    return response;
+  }
 }
 module.exports=Mailer;
