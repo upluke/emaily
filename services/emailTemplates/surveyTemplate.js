8052777736eabfb7d2be236c7c1cc874a8586c01
upluke <lukeforadream@gmail.com>
2019-08-26 21:04:00 -0700

    surveyTemplateUpdate

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,19 @@
 module.exports = (survey) => {
-    return '<div>' + survey.body + '</div>'
+    return `
+      <html>
+        <body>
+          <div style="text-align:center;">
+            <h3>Check it out!</h3>
+            <p>Plz anser the following questions below:</p>
+            <p>${survey.body}</p>
+            <div>
+              <a href="http://localhost:3000">Yes</a>
+            </div>
+            <div>
+              <a href="http://localhost:3000">No</a>
+            </div>
+          </div>
+        </body>
+      </html>
+    `
 };
