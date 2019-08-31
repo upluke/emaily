b76639b8ce88e1e82a3189fb2180092cb51d17a7
upluke <lukeforadream@gmail.com>
2019-08-30 21:22:16 -0700

    cssIcon

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 
 const Dashboard =()=>{
   return(
     <div>
       Dashboard ok
+      <div className="fixed-action-btn">
+        <Link to='/surveys/new' className="btn-floating btn-large green">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
     </div>
   )
 }
