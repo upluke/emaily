73f6e941abdb88f59c97729f8ea45949b0b87c70
upluke <lukeforadream@gmail.com>
2019-09-24 22:37:56 -0700

    reviewButton

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,16 @@
+// SurveyFormReview shows users their form inputs for reveiw
 import React from 'react';
 
-const SurveyFormReview =()=>{
+const SurveyFormReview =({onCancel})=>{
   return(
     <div>
       <h5>Please confirm your entries</h5>
+      <button
+        className="yellow darken-3 btn-flat"
+        onClick={onCancel}
+      >
+         Back
+      </button>
     </div>
   )
 }
