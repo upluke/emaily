939db6c1b79047b54d1888000a265ab421a7e697
upluke <lukeforadream@gmail.com>
2019-10-06 11:30:24 -0700

    formFields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,19 +4,12 @@ import {reduxForm, Field} from 'redux-form'; // is more like connect helper
 import {Link} from 'react-router-dom'
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails'
-
-
-const FIELDS=[
-  {label:'Survey Title', name:'title'},
-  {label:'Subject Line', name:'subject'},
-  {label:'Email Body', name:'body'},
-  {label:'Recipient List', name:'emails'}
-];
+import formFields from './formFields';
 
 
 class SurveyForm extends Component {
   renderFields(){
-    return FIELDS.map(({label, name})=>{
+    return formFields.map(({label, name})=>{
       return (
         <Field key={name} component={SurveyField} type="text" label={label} name={name}  />
       );
@@ -46,7 +39,7 @@ function validate(values) {
   const errors={};
   // reudxForm automatically matches up the errors are returning from that function to the different fields you're rendering
     errors.emails=validateEmails(values.emails|| '')
-    FIELDS.forEach(({name})=>{
+    formFields.forEach(({name})=>{
      if(!values[name]){
        errors[name]='You must provide a value';
      }
