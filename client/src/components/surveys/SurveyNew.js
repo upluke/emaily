c9c4d968f2dd7c7863c757fa765dbb33e31ac175
upluke <lukeforadream@gmail.com>
2019-10-12 12:01:02 -0700

    clearDataOnCancel

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,5 +1,6 @@
 // surveyNew shows surveyForm and surveyFormReview
 import React, {Component} from 'react'
+import {reduxForm} from 'redux-form';
 import SurveyForm from './SurveyForm'
 import SurveyFormReview from './SurveyFormReview'
 
@@ -28,4 +29,6 @@ class SurveyNew extends Component {
 }
 
 
-export default SurveyNew;
+export default reduxForm({
+  form:'surveyForm'
+})(SurveyNew);
